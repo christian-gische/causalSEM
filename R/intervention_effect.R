21aa7d976bd37ffc82deb0598d4a514bf8fddf7c
Christian Gische <gischekr@gmail.com>
2021-09-01 11:53:22 +0200

    changes we made during our zoom session on July, 30th

--- R/intervention_effect.R
+++ R/intervention_effect.R
@@ -5,17 +5,30 @@ intervention_effect <- function(model, intervention, outcome = NULL, interventio
 # check if arguments are well-specified FORMALLY; 
 # give warnings, error, etc.
 # later task
+# Check of supported class
+# Check if all variables are "manifest"
+# Check of verbosity argument 
+# using (or merging) the verbose_argument_handling function
+# todo: allow object of class causalSEM as input to the 
+# intervention effect function
   
 check_arguments(model, intervention, outcome, levels, effect.type, 
-                lower.bound, upper.bound, ...)  
+                lower.bound, upper.bound, verbose ...)  
   
 
 # creates empty list
 internal_list <- make_empty_list()
   
 # fills model info into list
+# todo: fills two slots of internal list: 
+# 1. fitted_object
+# 2. fitted_object_class
+
+
 internal_list <- populate_model_info(internal_list = internal_list, 
-                                     model = model, )
+                                     model = model)
+                                     
+                                     
   
 # fills intervention info into list
 internal_list <- populate_intervention_info(internal_list = internal_list, 
@@ -25,6 +38,7 @@ internal_list <- populate_intervention_info(internal_list = internal_list,
                                             effect.type = effect.type, 
                                             lower.bound = lower.bound,
                                             upper.bound = upper.bound )
+
 
 # build matrix of structural coefficients
 internal_list <- build_c(internal_list = internal_list) 
