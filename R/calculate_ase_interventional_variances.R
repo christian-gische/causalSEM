81e60917ccd39e791e5f896ff957161aba272ae4
Manuel Arnold <arnoldmz@hu-berlin.de>
2021-11-19 18:34:36 +0100

    added calculate_jacobian_interventional_means/variances/probabilities and calculate_ase_interventional_means/variances/probabilities

---
+++ R/calculate_ase_interventional_variances.R
@@ -0,0 +1,118 @@
+## Changelog:
+# MA 0.0.1 2021-11-19: initial programming
+
+## Documentation
+#' @title Calculate asymptotic standard errors of the interventional variances
+#' @description Internal functions that calculates the asymptotic covariance,
+#' standard errors, and z-values of the interventional variances.
+#' @param model internal_list or object of class causalSEM
+#' @param x interventional level
+#' @param intervention_names names of interventional variables
+#' @param outcome_names name of outcome variable
+#' @param verbose verbosity of console outputs
+#' @return \code{calculate_ase_interventional_variances} returns the
+#' asysmptotic covariance matrix, the aysmptotic standard errors, and the
+#' approximate z-values of the the interventional variances for a specific
+#' interventional level and a specific value in the range of the outcome
+#' variable (see Corollaries, 10, 11 in Gische and Voelkle, 2021).
+#' @references
+#' Gische, C. & Voelkle, M. C. (under review). Beyond the mean: A flexible framework for
+#'    studying causal effects using linear models. \url{https://www.researchgate.net/profile/Christian-Gische/publication/335030449_Gische_Voelkle_Causal_Inference_in_Linear_Models/links/6054eb6e299bf1736755110b/Gische-Voelkle-Causal-Inference-in-Linear-Models.pdf}
+
+calculate_ase_interventional_variances <- function(model, x, intervention_names, outcome_names, verbose) {
+
+  # function name
+  fun_name <- "calculate_ase_interventional_variances"
+
+  # function version
+  fun_version <- "0.0.1 2021-11-19"
+
+  # function name+version
+  fun_name_version <- paste0(fun_name, " (", fun_version, ")")
+
+  # get verbose argument
+  verbose <- model$control$verbose
+
+  # console output
+  if (verbose >= 2) {
+    cat(paste0("start of function ", fun_name_version, " ", Sys.time(), "\n"))
+  }
+
+  # TODO check if user argument model is the internal_list or
+  # an object of class causalSEM
+  # CURRENTLY, the function assumes that the input model is
+  # of type internal_list. After allowing for objects of class causalSEM
+  # the pathes starting with internal_list$ might need adjustment
+
+
+  # get variable names of interventional variables
+  if (is.character(intervention_names) && all(intervention_names %in% model$info_model$var_names)) {
+    x_labels <- intervention_names
+  } else {
+    x_labels <- model$info_interventions$intervention_name
+  }
+
+  # get interventional levels
+  if (is.numeric(x) && length (x) == length(intervention_names)) {
+    x_values <- x
+  } else {
+    x_values <- model$info_interventions$intervention_level
+  }
+
+  # get total number of variables
+  # get number of unique parameters
+  n <- model$info_model$n_ov
+  n_unique <- model$info_model$param$n_par_unique
+
+  # get intervential means
+  # TODO: assign E by calling the function calculate_interventional_means
+  gamma_2 <- model$interventional_distribution$moments$variance_matrix
+
+  # compute jacobian of the pdf
+  jac_g2 <- calculate_jacobian_interventional_variances(
+    model = model,
+    intervention_names = intervention_names,
+    outcome_names = outcome_names,
+    verbose = verbose
+  )
+
+  jac_g2_vec <- K_n %*% (G_2C %*% jac_C + G_2Psi %*% jac_Psi)
+
+  # get AV of parameter vector
+  acov <- model$info_model$param$varcov_par_unique
+
+  # compute asymptotic variance
+  #acov_gamma_2 <- jac_g2 %*% acov %*% t(jac_g2)
+  acov_gamma_2_vec <- jac_g2_vec %*% acov %*% t(jac_g2_vec)
+
+  labels_AV_gamma_2_vec <- expand.grid(model$info_model$var_names,model$info_model$var_names)
+  labels_AV_gamma_2_vec <- paste0(labels_AV_gamma_2_vec$Var1,labels_AV_gamma_2_vec$Var2)
+
+  AV_gamma_2_vec <- as.matrix(AV_gamma_2_vec)
+  colnames(AV_gamma_2_vec) <- labels_AV_gamma_2_vec
+  rownames(AV_gamma_2_vec) <- labels_AV_gamma_2_vec
+
+  variance_labels <- paste0(model$info_model$var_names,model$info_model$var_names)
+
+  acov_variances <- AV_gamma_2_vec[variance_labels,variance_labels]
+
+  # compute asymptotic standard errors
+  ase_gamma_2 <- sqrt(diag(acov_variances))
+
+  # compute approximate z-value
+  z_gamma_2 <- gamma_2 / ase_gamma_2
+
+  # Prepare output ----
+
+  # Console output
+  if (verbose >= 2) {
+    cat(paste0("  end of function ", fun_name_version, " ", Sys.time(), "\n"))
+  }
+
+  # Output
+  list(gamma_2 = gamma_2,
+       acov_gamma_2 = acov_variances,
+       ase_gamma_2 = ase_gamma_2,
+       z_gamma_2 = z_gamma_2)
+
+}
--- R/calculate_jacobian_interventional_variances.R
+++ R/calculate_jacobian_interventional_variances.R
@@ -1,35 +1,34 @@
 ## Changelog:
-# MA 0.0.2 2021-11-19: function makes use of calculate_zero_one_matrices and
-#                      other small changes. Should accept scalar and vector x now.
-# CG 0.0.1 2021-11-10: initial programming
+# MA 0.0.1 2021-11-19: Initial programming
 
 ## Documentation
-#' @title Calculate Jacobian of the interventional means for a specific
+#' @title Calculate Jacobian of the interventional variances for a specific
 #' interventional level and a specific value in the range of the outcome variable
 #' @description Internal function that calculates the Jacobian of the
-#' interventional means for a specific interventional level and a specific value
-#' in the range of the outcome variable.
+#' interventional variances for a specific interventional level and a specific value.
+#' in the range of the outcome variable
 #' @param model internal_list or object of class causalSEM
-#' @param x interventional level
 #' @param intervention_names names of interventional variables
 #' @param outcome_names name of outcome variable
 #' @param verbose verbosity of console outputs
-#' @return \code{calculate_jacobian_interventional_means} returns the
-#'    Jacobian of interventional mean vector (numeric matrices)
-#'    as defined in Eq. 18a (p. 17)
+#' @return \code{calculate_jacobian_interventional_variances} returns the
+#'    Jacobian of interventional covariance matrix (numeric matrices)
+#'    as defined in Eq. 18b (p. 17)
 #' @references
 #' Gische, C. & Voelkle, M. C. (under review). Beyond the mean: A flexible framework for
 #'    studying causal effects using linear models. \url{https://www.researchgate.net/profile/Christian-Gische/publication/335030449_Gische_Voelkle_Causal_Inference_in_Linear_Models/links/6054eb6e299bf1736755110b/Gische-Voelkle-Causal-Inference-in-Linear-Models.pdf}
 #' @keywords internal
 
 ## Function definition
-calculate_jacobian_interventional_means <- function(model, x, intervention_names, outcome_names, verbose){
+calculate_jacobian_interventional_variances <- function(
+  model, intervention_names, outcome_names, verbose
+  ) {
 
   # function name
-  fun_name <- "calculate_jacobian_interventional_means"
+  fun_name <- "calculate_jacobian_interventional_variances"
 
   # function version
-  fun_version <- "0.0.2 2021-11-19"
+  fun_version <- "0.0.1 2021-11-19"
 
   # function name+version
   fun_name_version <- paste0(fun_name, " (", fun_version, ")")
@@ -40,42 +39,55 @@ calculate_jacobian_interventional_means <- function(model, x, intervention_names
 
   # Prepare elements ----
 
-  # Calculate zero one matrices
+  # Number of observed variables
+  n <- model$info_model$n_ov
+
+  # Identity matrices
+  I_n <- diag(n)
+  I_n2 <- diag(n^2)
+
+  # # Calculate zero one matrices
   zero_one_matrices <- calculate_zero_one_matrices(
     model = model,
     intervention_names = intervention_names,
     outcome_names = outcome_names,
     verbose = verbose
-    )
-
-  # Create identity matrix
-  I_n <- diag(model$info_model$n_ov)
+  )
 
   # Selection
   ONE_I <- zero_one_matrices$select_intervention
   I_N <- zero_one_matrices$eliminate_intervention
 
+  # Elimination, duplication, and commutation matrices
+  L_n <- zero_one_matrices$elimination_matrix
+  D_n <- zero_one_matrices$duplication_matrix
+  K_n <- zero_one_matrices$commutation_matrix
+
   # C and Psi matrices
   C <- model$info_model$C$values
+  Psi <- model$info_model$Psi$values
 
   # Jacobian matrices
   jac_C <- model$info_model$C$derivative
-
-  # Asymptotic covariance matrix of the model parameters
-  acov <- model$info_model$param$varcov_par_unique
+  jac_Psi <- model$info_model$Psi$derivative
 
   # Compute transformation matrix
-
   C_trans <- solve(I_n - I_N %*% C)
 
 
-  # Calculate Jacobian g1 ----
-  # TODO: allow x to be a vector
+  # Calculate Jacobian of the interventional variances ----
 
-  jac_g1 <- kronecker(X = t(x) %*% t(ONE_I) %*% t(C_trans), Y = C_trans %*% I_N) %*% jac_C
+  G_2C <- (I_n2 + K_n) %*%
+    kronecker(X = C_trans %*% I_N %*% Psi %*% I_N, Y = I_n) %*%
+    kronecker(X = t(C_trans), Y = C_trans %*% I_N)
 
-  # return jacobian matrix
-  jac_g1
+  G_2Psi <- kronecker(X = C_trans, Y = C_trans) %*% kronecker(X = I_N, Y = I_N)
+
+  jac_g2 <- L_n %*% (G_2C %*% jac_C + G_2Psi %*% jac_Psi)
+
+  jac_g2
+
+  # Should the output be vectorized or not?
 
 }
 
