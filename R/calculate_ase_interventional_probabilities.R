ddf45cd238803e27deb073aa7a218ae87b3d0a44
Manuel Arnold <arnoldmz@hu-berlin.de>
2021-11-22 19:36:59 +0100

    added fill_in_asymptotics_means/variances/probabilities

--- R/calculate_ase_interventional_probabilities.R
+++ R/calculate_ase_interventional_probabilities.R
@@ -21,7 +21,7 @@
 #' Gische, C. & Voelkle, M. C. (under review). Beyond the mean: A flexible framework for
 #'    studying causal effects using linear models. \url{https://www.researchgate.net/profile/Christian-Gische/publication/335030449_Gische_Voelkle_Causal_Inference_in_Linear_Models/links/6054eb6e299bf1736755110b/Gische-Voelkle-Causal-Inference-in-Linear-Models.pdf}
 
-calculate_ase_interventional_means <- function(
+calculate_ase_interventional_probabilities <- function(
   model, x, intervention_names, outcome_names, lower_bound, upper_bound, verbose
   ) {
 
---
+++ R/fill_in_asymptotics_interventional_means.R
@@ -0,0 +1,70 @@
+## Changelog:
+# MA 0.0.1 2021-11-22: initial programming
+
+## Documentation
+#' @title Function to fill in asymptotic quantities of the interventional means
+#' @description Internal function that fills in the jacobian, asymptotic
+#' covariance matrix, asymptotic standard errors, and asymptotic z-values of the
+#' interventional means into the internal list.
+#' @param internal_list A list with various information extracted from the model.
+#' @return \code{fill_in_asymptotics_interventional_means} returns the inputted
+#' internal_list with several slots in ..$interventional_distribution filled.
+#'    ..$jacobian
+#'    ..$acov
+#'    ..$ase
+#'    ..$z_value
+#' @seealso \code{\link{}}
+#' @references
+#' Gische, C. & Voelkle, M. C. (2021). Beyond the mean: A flexible framework for
+#'    studying causal effects using linear models. Psychometrika.
+#' @keywords internal
+
+## Function definition
+fill_in_asymptotics_interventional_means <- function(internal_list){
+
+  # function name
+  fun_name <- "fill_in_asymptotics_interventional_means"
+
+  # function version
+  fun_version <- "0.0.1 2021-11-22"
+
+  # function name+version
+  fun_name_version <- paste0(fun_name, " (", fun_version, ")")
+
+  # get verbose argument
+  verbose <- internal_list$control$verbose
+
+  # console output
+  if(verbose >= 2) cat(paste0( "start of function ", fun_name_version, " ", Sys.time(), "\n" ))
+
+  # calculate jacobian
+  jacobian <- calculate_jacobian_interventional_means(
+    model = internal_list,
+    x = internal_list$info_interventions$intervention_level,
+    intervention_names = internal_list$info_interventions$intervention_name,
+    outcome_names = internal_list$info_interventions$outcome_name,
+    verbose = verbose
+  )
+
+  # calculate asym
+  ase <- calculate_ase_interventional_means(
+    model = internal_list,
+    x = internal_list$info_interventions$intervention_level,
+    intervention_names = internal_list$info_interventions$intervention_name,
+    outcome_names = internal_list$info_interventions$outcome_name,
+    verbose = verbose
+  )
+
+  # fill in slots of ...$interventional_distribution
+  internal_list$interventional_distribution$jacobian$means <- jacobian
+  internal_list$interventional_distribution$acov$means <- ase$acov_gamma_1
+  internal_list$interventional_distribution$ase$means <- ase$ase_gamma_1
+  internal_list$interventional_distribution$z_values$means <- ase$z_gamma_1
+
+  # console output
+  if(verbose >= 2) cat( paste0("  end of function ", fun_name_version, " ", Sys.time(), "\n" ))
+
+  # return internal list
+  internal_list
+
+}
